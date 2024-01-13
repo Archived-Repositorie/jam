d20758daae9b049b24e8c8daf8c2308b9892aef5
kittech <kittech@skiff.com>
2024-01-13 15:37:51 +0100

    base

---
+++ src/main.rs
@@ -0,0 +1,20 @@
+use clap::Parser;
+
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+struct Args {
+    #[arg(short, long)]
+    name: String,
+
+    /// Number of times to greet
+    #[arg(short, long, default_value_t = 1)]
+    count: u8,
+}
+
+fn main() {
+    let args = Args::parse();
+
+    for _ in 0..args.count {
+        println!("Hello {}!", args.name)
+    }
+}
